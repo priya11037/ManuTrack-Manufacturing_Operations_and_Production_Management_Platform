853598487daea9b22d981429b15f147f2f173f84
gunasaisudheerthi-bangaru <Bangaru.GunasaiSudheerthi@cognizant.com>
2026-06-01 17:56:38 +0530

    intial commit

--- ManuTrack.UI/src/app/shared/shell/shell.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-shell',
-  templateUrl: './shell.component.html',
-  standalone: false
-})
-export class ShellComponent {}
